01552c627ac800ada8dfb599a151cae3485118e8
Aurelien Gateau <mail@agateau.com>
2024-04-25 09:19:52 +0200

    Add test for metainfo module

---
+++ tests/data.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DATA_DIR = Path(__file__).parent / "data"
+
+TEST_VIDEO_PATH = DATA_DIR / "PXL_20240425_064525642.mp4"
---
+++ tests/test_metainfo.py
@@ -0,0 +1,30 @@
+from datetime import timedelta
+
+import arrow
+from data import TEST_VIDEO_PATH
+
+from pvmetatools import metainfo
+
+
+def test_read():
+    dct = metainfo.read(str(TEST_VIDEO_PATH))
+
+    creation_time = arrow.get("2024-04-25T08:45:27+02:00")
+
+    assert dct == {
+        "filename": str(TEST_VIDEO_PATH),
+        "nb_streams": 2,
+        "nb_programs": 0,
+        "format_name": "mov,mp4,m4a,3gp,3g2,mj2",
+        "format_long_name": "QuickTime / MOV",
+        "start_time": "0.000000",
+        "duration": timedelta(microseconds=589500),
+        "size": 1228124,
+        "bit_rate": 16666653,
+        "probe_score": 100,
+        "major_brand": "isom",
+        "minor_version": "131072",
+        "compatible_brands": "isomiso2mp41",
+        "creation_time": creation_time,
+        "com.android.capture.fps": "30.000000",
+    }
